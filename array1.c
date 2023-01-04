01f3953261e260db6a71b96165c84ba959b1ca02
bhavana1205 <121919612+bhavana1205@users.noreply.github.com>
2023-01-04 10:14:49 +0530

    Add files via upload

---
+++ array1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define max 10
+void main()
+{
+	int a[max][max];
+	int rows,cols,j,i;
+	printf("enter the value of rows,cols");
+	scanf("%d",&rows,&cols);
+	{
+	printf("enter the value rows*cols");
+	for(i=0;i<rows;i++);
+	for(j=0;j<cols;j++);
+		scanf("%d",&a[i][j]);
+	}
+printf("\n");
+}
